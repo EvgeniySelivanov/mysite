35a808e42099a1bea915dbcdd62e68e661749b8a
Evgeniy Selivanov <genyaselivanovzp@gmail.com>
2022-12-09 16:19:23 +0200

    добавил фон с странице портфолио исправил счетчик количества проектов

--- wp-content/themes/mytheme/functions.php
+++ wp-content/themes/mytheme/functions.php
@@ -5,17 +5,19 @@ require_once $_SERVER['DOCUMENT_ROOT'].'/mysite/wp-content/themes/mytheme/bstr/b
 
 //подключаю стили правильно
 function it_blog_style_frontend() {
-   wp_enqueue_style('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css');
+   wp_enqueue_style('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css');
    wp_enqueue_style('styles', get_stylesheet_directory_uri() . '/css/style.css');
    wp_enqueue_style('styles2', get_stylesheet_directory_uri() . '/css/news-page-template.css');
    wp_enqueue_style('styles3', get_stylesheet_directory_uri() . '/css/contact-page-template.css');
+   wp_enqueue_style('styles4', get_stylesheet_directory_uri() . '/css/portfolio-page-template.css');
+
 }
 //вызвал встроенный хук и подключил стили
 add_action('wp_enqueue_scripts', 'it_blog_style_frontend');
 //подключаю скрипты
 function it_blog_include_myscript(){
   
-   wp_enqueue_script('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js', '', '1.0', true);
+   wp_enqueue_script('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js', '', '1.0', true);
 }
 //вызвал встроенный хук и подключил  скрипт 
 add_action('wp_enqueue_scripts', 'it_blog_include_myscript');
--- wp-content/themes/mytheme/templates/page-portfolio.php
+++ wp-content/themes/mytheme/templates/page-portfolio.php
@@ -5,8 +5,8 @@
  */?>
 <?php get_header();?>
 <?php
-//количество постов этого типа(сущности)
-  $numberWork=wp_count_posts()->publish;
+//количество постов этого типа(сущности project_list)
+  $numberWork=wp_count_posts('project_list')->publish;
   $current_page = (get_query_var('paged')) ? get_query_var('paged') : 1;
   $params = array(
     'posts_per_page' => 3, // количество постов на странице
@@ -23,35 +23,43 @@
  //var_dump($posts); 
 
 ?>
-<div class="container"> 
-  <div><strong>общее количество работ:<?php echo $numberWork;?></strong> </div> 
-  <table class="table">
-  <thead>
-      <tr>
-        <th scope="col">Date</th>
-        <th scope="col">Name</th>
-        <th scope="col">Framework</th>
-        <th scope="col">Description</th>
-      </tr>
-    </thead>
-    <tbody>
-  <?php
+<div class="myTemplatePageContent bg-portfolio ">
+  <div class="container">
+    
+    <table class="table">
+    <thead>
+        <tr>
+          <th scope="col">Date</th>
+          <th scope="col">Name</th>
+          <th scope="col">Framework</th>
+          <th scope="col">Description</th>
+        </tr>
+      </thead>
+      <tbody class="bg-secondary bg-opacity-75">
+    <?php
+  
+    while (have_posts()) : the_post();
+    ?>
+        <tr>
+          <!--Вывод даты с помощь функции get_field| date это поле из ACF -->
+          <th scope="row" class="fs-5"><?php echo get_field('date')?></th>
+          <td><a class="link-info fs-5 fw-bold" href="<?php echo get_field('address')?>"  target="_blank" ><?php echo get_field('address')?></a></td>
+          <td class="text-warning fs-5 fw-bold"><?php echo get_field('tehnology_type')?></td>
+          <td><?php the_content();?></td>
+        </tr>
+       <?php endwhile;?>
+      </tbody>
+    </table>
+    <div class="d-flex flex-row justify-content-between align-items-center">
+      <div>
+        <strong>Total number of projects:<?php echo $numberWork;?></strong> 
+      </div>
+      <div class="d-flex justify-content-center mt-2">
+        <?php do_action('myPostPagination');?>
+      </div>
+    </div>
 
-  while (have_posts()) : the_post();
-  ?>
-      <tr>
-        <!--Вывод даты с помощь функции get_field| date это поле из ACF -->
-        <th scope="row"><?php echo get_field('date')?></th>
-        <td><a href="<?php echo get_field('address')?>"><?php echo get_field('address')?></a></td>
-        <td><?php echo get_field('tehnology_type')?></td>
-        <td><?php the_content();?></td>
-      </tr>
-     <?php endwhile;?>
-    </tbody>
-  </table>
-  <div class="d-flex justify-content-center mt-2">
-      <?php do_action('myPostPagination');?>
-</div>
+  </div>
 </div>
 
 
